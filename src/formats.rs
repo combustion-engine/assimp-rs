745d8cf014c175265565ea4f218338577ac78828
Aaron Trent <novacrazy@gmail.com>
2017-02-05 04:35:43 -0600

    Add is_extension_supported function that calls ffi::aiIsExtensionSupported

--- src/formats.rs
+++ src/formats.rs
@@ -2,7 +2,18 @@
 //!
 //! See http://www.assimp.org/main_features_formats.html
 
+use std::ffi::CString;
+
 use phf;
+
+use ::ffi;
+
+/// Calls `aiIsExtensionSupported` to check if Assimp can handle the extension
+pub fn is_extension_supported(ext: &str) -> bool {
+    let c_str = CString::new(ext).unwrap();
+
+    unsafe { ffi::aiIsExtensionSupported(c_str.as_ptr()) == ffi::TRUE }
+}
 
 /// Contains all Assimp file formats and their extensions
 ///
@@ -150,5 +161,12 @@ mod test {
         for ext in PARTIALLY_SUPPORTED_IMPORT_EXTENSIONS.iter() {
             assert!(IMPORT_EXTENSIONS.contains_key(ext));
         }
+    }
+
+    #[test]
+    fn test_supported() {
+        assert!(is_extension_supported("obj"));
+        assert!(is_extension_supported("ply"));
+        assert!(is_extension_supported("dae"));
     }
 }
