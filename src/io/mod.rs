c470c5898743645aafc0459e9892467fa55a58fb
Aaron Trent <novacrazy@gmail.com>
2017-02-07 07:01:11 -0600

    Remove streaming file io stuff because it was too overreaching and would be better implemented in the dependent crate

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -2,15 +2,9 @@
 mod macros;
 
 pub mod default;
-pub mod stream;
-pub mod read_only;
-pub mod multi;
 
 pub trait AssimpIO<'a> {
     fn get(&'a mut self) -> &'a mut ::ffi::AiFileIO;
 }
 
 pub use self::default::DefaultIO;
-pub use self::stream::StreamIO;
-pub use self::read_only::ReadOnlyStreamIO;
-pub use self::multi::{MultiStreamIO, to_stream, to_write_stream};
